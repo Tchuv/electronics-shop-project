3818d282874c508e89d296a7905d8199a3291752
Tchuv <tchuvashov1706@gmail.com>
2023-03-19 18:52:22 +1200

    Добавил в класс `Item`: атрибут `name` сделать приватным - добавить геттер и сеттер для `name`, используя @property - в сеттере `name` проверять, что длина наименования товара не больше 10 симвовов

--- src/item.py
+++ src/item.py
@@ -13,7 +13,7 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self.name = name
+        self.__name = name
         self.price = price
         self.quantity = quantity
         Item.all.append(self)
@@ -33,3 +33,17 @@ class Item:
         """
 
         self.price = self.price * self.pay_rate
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, name):
+        if len(name) <11:
+            self.__name = name
+
+
+
+
+
